6089cf504fa7087a7a0be48bd0713faa0cfa0007
madiyar <madiyar.bolatov97@gmail.com>
2021-07-04 13:01:08 +0600

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,6 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {},
-    colors: {
-      primary: '#DC780B',
-      light: '#FBF1E7',
-      black: 'black',
-      secondary: '#150C01',
-      dark: '#252525',
-      white: 'white',
-      gray: '#EDEDED'
-    },
-    fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
-      title: ['Philosopher', 'sans-serif']
-    },
   },
   variants: {
     extend: {},
